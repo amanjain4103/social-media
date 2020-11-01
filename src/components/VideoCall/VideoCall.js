463eda63e9fba72fb9804db5b65ce77b53949d0b
amanjain4103 <amanjain4103@gmail.com>
2020-11-01 19:58:47 +0530

    troubleshooted video call but didn't work

--- src/components/VideoCall/VideoCall.js
+++ src/components/VideoCall/VideoCall.js
@@ -9,9 +9,20 @@ const VideoCall = ({roomId, endVideoCall, socketForVideoCall}) =>  {
     const [{socket},] = useStateValue();
     const BASE_URL = process.env.REACT_APP_BASE_URL;
     const [currentVideo, setCurrentVideo] = useState();
+    
+
+    // peer initialization
+    const peer = new Peer(undefined, {
+        path:"/peerjs",
+        host: "/", 
+        port:"8000" //node server port
+    })
 
     // temporary for testing
     useEffect(() => {
+
+        const myVideo = document.getElementById("video__videoGrid__myVideo");
+        const friendsVideo = document.getElementById("video__videoGrid__friendsVideo")
 
         roomId = "aman@gmail.com";
         // endVideoCall => in chat.js => just console logging right now
@@ -27,83 +38,72 @@ const VideoCall = ({roomId, endVideoCall, socketForVideoCall}) =>  {
 
         newSocket.emit("join-video-call",roomId, 10);
 
-        return () => newSocket?.close();
-
-    },[])
-
-    const peer = new Peer(undefined, {
-        host: "/",
-        port:"3000"
-    })
-
     // streaming videos
-    useEffect(() => {
 
         // streaming my own video
-        const myVideo = document.getElementById("video__videoGrid__myVideo");
-        const friendsVideo = document.getElementById("video__videoGrid__friendsVideo")
-
         navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true
         }).then((stream) => {
-    
-            // myVideoStream = stream;
-            myVideo.srcObject = stream;
-            myVideo.onloadedmetadata = (e) => {
-                myVideo.play();
-            }
 
-            // addVideoStream(myVideo, stream);
-        }).catch((err) => {
-            console.log(err);
+            // myVideoStream = stream;
+            addVideoStream(myVideo, stream)
+            peer.on("call",call => {
+                call.answer(stream);
+                call.on("stream",friendsVideoStream => {
+                    addVideoStream(friendsVideo,friendsVideoStream);
+                })
+            })
+
+            newSocket.on("user-connected-on-video-call", (userId) => {
+                connectToNewUser(userId,stream,friendsVideo);
+            })
+
         })
+        // .catch((err) => {
+        //     console.log(err);
+        // })
+        
+        peer.on("open", id => {
+            // console.log("peerid-frontend "+id);
+            newSocket.emit("join-room-on-video-call",roomId, id);
+        })
+
+        // clearing the socket on exit
+        return () => newSocket?.close();
+
     
     }, [])
 
+    const connectToNewUser = (userId, stream, friendsVideo) => {
+        console.log(userId)
+        const call = peer.call(userId, stream);
+        // video element to render a call
+        call.on("stream",friendsVideoStream => {
+            addVideoStream(friendsVideo, friendsVideoStream);
+        })
+
+        call.on('close', () => {
+            console.log("stream closed")
+        })
+    }
+
     const addVideoStream = (video, stream) => {
+        console.log(video)
         video.srcObject = stream;
         video.addEventListner("loadmetadata", () => {
             video.play();
-            setCurrentVideo(video);
         })
-        // videoGrid?.append(video);
     }
 
-    // 59.45
-
-    // const myVideo = document.createElement("video");
-    // myVideo.muted = true;
-    // const peers = {};
-    // navigator.mediaDevices.getUserMedia({
-    //     video: true,
-    //     audio: true
-    // }).then(stream => {
-    //     addVideoStream(myVideo, stream)
-
-    //     peer.on("call", call => {
-    //         call.answer(stream);
-    //         const video = document.createElement("video")
-    //         call.on("stream",userVideoStream => {
-    //             addVideoStream(video, userVideoStream)
-    //         })
-    //     })
-
-    //     socket.on('user-connected', userId => {
-    //         connectToNewUser(userId, stream)
-    //       })
-
-        
-
-
-    // } )
+    // 1.32
 
     return (
         <div>
             {/* video of video call */}
             <button onClick={() => console.log(socket)} >click</button>
             <div id="video__videoGrid">
-                <video id="video__videoGrid__myVideo" width="500" height="500" autoPlay={true} muted={true} />
+                <video id="video__videoGrid__myVideo" width="300" height="300" autoPlay={true} muted={true} />
                 <video id="video__videoGrid__friendsVideo" width="300" height="300" autoPlay={true} muted={false} />
             </div>
         </div>
